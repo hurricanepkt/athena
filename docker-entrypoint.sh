240bfd3038e17e4d258c862f5b1b20b709123ead
Mark Greenway <mark.greenway@LIGMarine.com>
2023-05-29 23:24:15 -0400

    webhook

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,7 +2,7 @@
 LOG_LOCATION=/var/www/html/MylogFile.log
 [ "$1" = "update" ] || exec "$@" || exit $?
 
-
+printf "Starting v0.9.7\n"  > $LOG_LOCATION; 
 lighttpd -D -f /etc/lighttpd/lighttpd.conf  &
 
 while [ : ]
