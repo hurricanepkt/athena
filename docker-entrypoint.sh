7ef879d2cbe22948506271bfcd8e67928009fbee
Mark Greenway <mark.greenway@LIGMarine.com>
2023-05-30 14:23:34 -0400

    append

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,22 +2,22 @@
 LOG_LOCATION=/var/www/html/MylogFile.log
 [ "$1" = "update" ] || exec "$@" || exit $?
 
-printf "Starting v0.9.7\n"  > $LOG_LOCATION; 
+printf "Starting v0.9.8\n"  >> $LOG_LOCATION; 
 lighttpd -D -f /etc/lighttpd/lighttpd.conf  &
 
 while [ : ]
 do
     FILES=/input_music/*
     for f in $FILES; do 
-        printf "$f\n"  > $LOG_LOCATION; 
+        printf "$f\n"  >> $LOG_LOCATION; 
         if [ $f = "/input_music/*" ]
         then
-            printf "No files to process\n" > $LOG_LOCATION;
+            printf "No files to process\n" >> $LOG_LOCATION;
             break
         fi
         printf "Splitting\n" > $LOG_LOCATION;
         mp3splt -q -s -p th=-50,nt=20 -d /output_music -o @f/@f_@n $f;
-        printf "Removing $f\n" > $LOG_LOCATION;
+        printf "Removing $f\n" >> $LOG_LOCATION;
         rm $f
     done
     sleep $SLEEP_DELAY
